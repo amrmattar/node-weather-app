ffacddf4777273dc3c744426c20f063fd78d67cb
amrmattar <71548986+amrrain1@users.noreply.github.com>
2021-08-06 21:17:25 +0200

    firs heroku1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 const port = process.env.PORT || 3000;
+
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
