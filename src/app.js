0c21694cec1d92ddb617ddf14862a8edb4a354fc
amrmattar <71548986+amrrain1@users.noreply.github.com>
2021-08-12 17:15:13 +0200

    segd

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000;
 
-
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
